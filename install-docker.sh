9d9afc71058aa3db19d9d209895260d79ac3e927
qujsh <ningmingqaz@126.com>
2019-04-28 11:18:07 +0800

    adjust install docker sh file

--- install-docker.sh
+++ install-docker.sh
@@ -1,20 +1,8 @@
 #!/bin/sh
 #description: install docker, create user
 
-#constant
 USERNAME="qujsh"
 PASSWORD=`echo ${USERNAME} | md5sum | cut -b 4-10 | tee ~/pass`
-
-NAMES="docker_ce docker_ce_cli containerd_io docker_compose"
-DOCKER_CE='https://download.docker.com/linux/centos/7/x86_64/stable/Packages/docker-ce-18.09.5-3.el7.x86_64.rpm'            #if it's too slow, you can set your own file store server
-DOCKER_CE_CLI='https://download.docker.com/linux/centos/7/x86_64/stable/Packages/docker-ce-cli-18.09.5-3.el7.x86_64.rpm'
-CONTAINERD_IO='https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.5-3.1.el7.x86_64.rpm'
-DOCKER_COMPOSE='https://github.com/docker/compose/releases/download/1.24.0/docker-compose-Linux-x86_64'                     #centos 64bit system docker-compose
-DOWNLOAD='/root/download'                                                   #download place
-DOCKER_PATH='/usr/bin/docker'
-DOCKER_COMPOSE_PATH='/usr/local/bin/docker-compose'
-REGISTRY_MIRROR='https://registry.docker-cn.com'                            #you need replace your own Mirror accelerator
-
 
 if [ $USER != "root" ]; then
 	echo "please use root account"
@@ -22,6 +10,16 @@ if [ $USER != "root" ]; then
 fi
 
 #INSTALL DOCKER USE PACKAGE
+NAMES="docker_ce_cli containerd_io docker_ce docker_compose"                                                                #order is important, they are interdependence
+DOCKER_CE='https://download.docker.com/linux/centos/7/x86_64/stable/Packages/docker-ce-18.09.5-3.el7.x86_64.rpm'            #if it's too slow, you can set your own file store server
+DOCKER_CE_CLI='https://download.docker.com/linux/centos/7/x86_64/stable/Packages/docker-ce-cli-18.09.5-3.el7.x86_64.rpm'
+CONTAINERD_IO='https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.5-3.1.el7.x86_64.rpm'
+DOCKER_COMPOSE='https://github.com/docker/compose/releases/download/1.24.0/docker-compose-Linux-x86_64'                     #centos 64bit system docker-compose
+DOWNLOAD='/root/download'                                                                                                   #download place
+DOCKER_PATH='/usr/bin/docker'
+DOCKER_COMPOSE_PATH='/usr/local/bin/docker-compose'
+REGISTRY_MIRROR='https://registry.docker-cn.com'                                                                            #you need replace your own Mirror accelerator
+
 if [ ! -d $DOWNLOAD ]; then
 	mkdir $DOWNLOAD
 fi
@@ -32,7 +30,7 @@ do
 		NAME=$(echo $name | tr 'a-z' 'A-Z')
 		eval tmp=$(echo \$$NAME)
 
-		cd $DOWNLOAD && wget -O $name.rpm $tmp      # suffix need to be .rpm, or yum can't be installed
+		cd $DOWNLOAD && wget -O $name.rpm $tmp
 
 		if [ $? != 0 ]; then
 			echo "download $name package failed"
@@ -41,12 +39,28 @@ do
 	fi
 done
 
-# execute this file and yum again，for some extra docker wrong
+# remove first, and do add
+yum list installed | grep docker | awk '{print $1}' | xargs yum remove -y
+yum list installed | grep containerd | awk '{print $1}' | xargs yum remove -y
+
+# set yum aliyun repos
+if [ ! -f /etc/yum.repos.d/CentOS-Base.repo.backup ];then
+	mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
+	wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+	yum makecache
+
+	if [ $? != 0 ]; then
+		echo 'yum repo set faild'
+		exit
+	fi
+fi
+
+
 #if [ ! -f $DOCKER_PATH ]; then
 	for name in $NAMES
 	do
-        	if [ $name == "docker_compose.rpm"  ]; then         # .rpm is common to all
-			mv $DOWNLOAD/$name.rpm $DOCKER_COMPOSE_PATH
+        	if [ $name == "docker_compose"  ]; then
+			cp $DOWNLOAD/$name.rpm $DOCKER_COMPOSE_PATH
 			chmod +x $DOCKER_COMPOSE_PATH
                 	break
 	        fi
@@ -56,6 +70,7 @@ done
 
 	if [ $? != 0 ]; then
 	        echo 'docker install failed'
+		exit
 	fi
 
 	systemctl enable docker
